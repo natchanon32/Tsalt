1083399715246d478c58747d78de1e220bbce8aa
natchanon32 <fgisgbj3629@gmail.com>
2025-07-20 13:46:46 +0700

    Create  position namw in class .py

---
+++ position namw in class .py
@@ -0,0 +1,12 @@
+import pandas as friend
+data = {
+    "r1": ["x","-","Job","Yuuna","Folk","Pond","Tonnam","Flank"],
+    "r2": ["x","Terd","Zick","Beam","Fill","Tor","Tin","K"],
+    "r3": ["x","Tae","Ja","Win","-","Prame","Pan","Boss"],
+    "r4": ["x","Ken","Palm","Korpak","Ashi","Ter","M","Chrome"],
+    "r5": ["x","-","Gun","-","Ji","Garfield","Champ","Wan"],
+    "r6": ["x","New","Yah","Pae","Beer","Chet","Oh","Leaf"],
+    "r7": ["x","Game","Mind","Dream","Arm","Mink","-","Non"],
+    "r8": ["x","Ten", "C", "Mint", "Biw", "Jo", "Nine", "-"],
+    }
+print(friend.DataFrame(data))
